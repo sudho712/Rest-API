39410d31a7b4c45dbb7376d33deda199c8527d5f
Sudhir Kumar <sudho712@gmail.com>
2024-11-18 00:06:45 +0530

    used routes to fetch data

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express")
 const app=express();
 
 const PORT=process.env.PORT || 3000;
+
+const products_routes = require("./routes/products")
 app.get("/",(req, res) => {
     res.send("hi i am live");
 });
+
+// middleware or to set router 
+app.use("/api/products", products_routes);
 
 const start = async()=>{
     try{
